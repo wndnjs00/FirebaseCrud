c1df65b7921b536cd0bfa21677ce3dd7e7d3e961
TodoList <juwonjeon852@gmail.com>
2024-01-26 21:58:16 +0900

    사용자 등록

--- app/src/main/java/com/example/firebasecrud/MainActivity.kt
+++ app/src/main/java/com/example/firebasecrud/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.firebasecrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.firebasecrud.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // viewbinding객체 생성
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 데이터베이스 클래스 객체생성
+        val dao = UserDao()
+
+
+        // addBtn 눌렀을때
+        binding.addBtn.setOnClickListener {
+
+            // 입력값 변수에 담기
+            val name = binding.nameEdit.text.toString()  //이름   //binding을 사용해서 nameEdit값을 가져오는데 String값으로 바꿔서 가져옴
+            val age = binding.ageEdit.text.toString()    //나이   //binding을 사용해서 ageEdit값을 가져오는데 String값으로 바꿔서 가져옴
+
+            // 입력값을 담은변수를 User클래스에 담기(데이터 세팅)
+            val user = User("", name, age)  // userKey,userName,userAge
+
+            // 위에 세팅된 값을 데이터베이스에 전송한다음 등록
+            dao.add(user)?.addOnSuccessListener {
+                // 데이터가 성공적으로 들어갔을때
+                Toast.makeText(this, "등록 성공!", Toast.LENGTH_SHORT).show()
+
+            }?.addOnFailureListener {
+                // 데이터 등록이 실패했을때
+                Toast.makeText(this, "등록 실패 : ${it.message}", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/example/firebasecrud/UserDao.kt
+++ app/src/main/java/com/example/firebasecrud/UserDao.kt
@@ -4,21 +4,23 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-// 데이터베이스와 연결해줄 클래스 생성
+// 실시간 데이터베이스와 연결해줄 클래스 생성
 
 class UserDao {
 
     private var databaseReference : DatabaseReference? = null
 
 
-    // init안에 있는 코드 = UserDao 객체 생성시에 바로 실행할 코드
+    // init안에 있는 코드 = UserDao 객체 생성시에 바로 실행될 코드
     init {
         val db = FirebaseDatabase.getInstance()
         //FirebaseDatabase에 있는 user에 저장
         databaseReference = db.getReference("user")
     }
 
+
     // 사용자 등록함수
+    // 사용자 정보를 전달받아 실시간 데이터베이스에 저장
     fun add(user : User?) : Task<Void>{
         // databaseReference객체를 통해서 push(값을넣어줌)를 해주는데, user값을 넣어줌
          return databaseReference!!.push().setValue(user)
